f3954c62db8d267e970056b7af399120b5dd079d
HYONEE <hellohk07@gmail.com>
2022-03-25 16:46:19 +0900

    Movie Review CSS complete

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,9 @@ import { FiArrowUp } from "react-icons/fi";
 import { MovieReview } from './MovieReview';
 
 export const Main = () => {
+  const scrollTop = () => {
+    window.scrollTo(0, 0)
+  }
 
   return (
     <>
@@ -17,7 +20,7 @@ export const Main = () => {
         </div>
       </section>
 
-      <div id='to-top'>
+      <div id='to-top' onClick={scrollTop}>
         <FiArrowUp size='20' />
       </div>
     </>
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
 import './Review.css'
 
 export const Review = () => {
 
   return (
     <>
-    <h1>안녕!!</h1>
+      <div className='form'>
+        <div className='inner' >
+            <h1>Movie Review</h1>
+            <div className='write'>
+
+              <input className='title-input' type='text' placeholder='Title' />
+              <div className='editor'>
+                <Editor
+                  initialValue="Write Now!"
+                  previewStyle="vertical"
+                  height="500px"
+                  initialEditType="markdown"
+                  useCommandShortcut={true}
+                />
+              </div>
+
+            </div>
+            <div className='btn-group'>
+              <div className='btn save'>저장</div>
+              <div className='btn btn--reverse cancel'>취소</div>
+            </div>
+        </div>
+      </div>
     </>
   )
 }
