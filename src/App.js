480854a7ee681bb6df00439b35cd80c2858e9434
HYONEE <hellohk07@gmail.com>
2022-03-29 17:52:36 +0900

    [WIP] Signin CSS

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Main } from './components/Main/Main'
 import { Review } from './components/Review/Review'
+import { Signin } from './components/Signin/Signin';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
             <Routes>
               <Route path='/' element={<Main />} />
+              <Route path='/signin/*' element={<Signin />} />
               <Route path='/review/*' element={<Review />} />
             </Routes>
         </BrowserRouter>
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signin.css'
 
 export const Signin = () => {
+  const idEl = document.querySelector('#id');
+  const pwEl = document.querySelector('#pw');
+  // const btnEl = document.querySelector('#btn');
+  const labelEl = document.querySelector('label');
+
+  // const warningHandler = (e) => {
+  //   console.log('click');
+  //   if (idEl.val() === '') {
+  //     idEl.next('label').addClass('warning');
+  //     setTimeout(() => {
+  //       labelEl.removeClass('warning');
+  //     }, 1500);
+  //   }
+  //   else if (pwEl.val() === '') {
+  //     pwEl.next('label').addClass('warning');
+  //   }
+  // }
+
+  const [loginInfo, setLoginInfo] = useState({ userId: '', password: '' });
+  const [addWarning, setAddWarning] = useState(false);
+
+  const getInputId = (e) => {
+    console.log('ID: ',e.target.value)
+    setLoginInfo({
+      userId: e.target.value,
+      password: loginInfo.password
+    });
+  }
+  const getInputPW = (e) => {
+    console.log('PW: ',e.target.value)
+    setLoginInfo({
+      userId: loginInfo.userId,
+      password: e.target.value
+    });
+  }
+
+  const warningHandler = (e) => {
+    console.log('click');
+    const { userId, password } = loginInfo;
+    // if (userId === '') {
+    //   setAddWarning(
+    //     idEl.next('label').addClass('warning')
+    //     .setTimeout(() => {
+    //       labelEl.remveClass('warning')
+    //     }, 1500)
+    //   )
+    // }
+    // else if (password === '') {
+    //   pwEl.next('label').addClass('warning');
+    // }
+    if (userId === ''|| password === '') {
+      setAddWarning(!addWarning)
+    }
+  }
+
 
   return (
     <>
-    <h1>로그인!</h1>
+      <section className='signin-form'>
+
+          <h1>Movie Review</h1>
+          <form action=''>
+            <div className='input-area'>
+              <input type='text' className='id' id='id' 
+              autoComplete='off' required
+              onChange={getInputId}
+              />
+              <label className={addWarning? 'warning' : ''} for='id'>User Name</label>
+            </div>
+            <div className='input-area'>
+              <input type='password' className='pw' id='pw' 
+              autoComplete='off' required
+              onChange={getInputPW}
+              />
+              <label className={addWarning? 'warning' : ''} for='pw'>Password</label>
+            </div>
+
+            <div className='btn-area'>
+              <button id='btn' type='submit' onClick={warningHandler}>Sign In</button>
+            </div>
+          </form>
+
+      </section>
     </>
   )
 }
